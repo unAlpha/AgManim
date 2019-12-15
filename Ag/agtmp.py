43e54d55b2318e5763db6722280de483811051e9
彭导 <pengyinzhong@192.168.0.109>
2019-12-16 00:00:35 +0800

    del all __pycache__ MacPro

--- Ag/agtmp.py
+++ Ag/agtmp.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # from manimlib.imports import *
-from active_projects.diffyq.part2.fourier_series import *
 from Ag.Grid import *
 
 class CoordScreen(Scene):
@@ -332,66 +331,6 @@ class PlotFunc(GraphScene):
         return np.cos(x)
     def func_sin(self, x):
         return np.sin(x)
-
-
-class FourierOfSigmaSymbol(FourierCirclesScene):
-    CONFIG = {
-        "n_vectors": 20,
-        "center_point": ORIGIN,
-        "slow_factor": 0.05,
-        "n_cycles": 1,
-        "tex": "\\pi",
-        "start_drawn": False,
-        "max_circle_stroke_width": 1
-    }
-
-    def construct(self):
-        self.add_vectors_circles_path()
-        for n in range(self.n_cycles):
-            self.run_one_cycle()
-
-    def add_vectors_circles_path(self):
-        path = self.get_path()
-        coefs = self.get_coefficients_of_path(path)
-        vectors = self.get_rotating_vectors(coefficients=coefs)
-        circles = self.get_circles(vectors)
-        self.set_decreasing_stroke_widths(circles)
-        # approx_path = self.get_vector_sum_path(circles)
-        drawn_path = self.get_drawn_path(vectors)
-        if self.start_drawn:
-            self.vector_clock.increment_value(1)
-
-        self.add(path)
-        self.add(vectors)
-        self.add(circles)
-        self.add(drawn_path)
-
-        self.vectors = vectors
-        self.circles = circles
-        self.path = path
-        self.drawn_path = drawn_path
-
-    def run_one_cycle(self):
-        time = 1 / self.slow_factor
-        self.wait(time)
-
-    def set_decreasing_stroke_widths(self, circles):
-        mcsw = self.max_circle_stroke_width
-        for k, circle in zip(it.count(1), circles):
-            circle.set_stroke(width=max(
-                # mcsw / np.sqrt(k),
-                mcsw / k,
-                mcsw,
-            ))
-        return circles
-
-    def get_path(self):
-        tex_mob = TexMobject(self.tex)
-        tex_mob.set_height(6)
-        path = tex_mob.family_members_with_points()[0]
-        path.set_fill(opacity=0)
-        path.set_stroke(WHITE, 1)
-        return path
 
 
 class SunAndPlanet(ThreeDScene):
