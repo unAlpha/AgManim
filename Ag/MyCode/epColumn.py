21da5ebab63a68b1365c174785eaf983fb1d6d81
unAlpha <pagzhong@gmail.com>
2020-01-10 18:24:37 +0800

    add ep05

--- Ag/MyCode/epColumn.py
+++ Ag/MyCode/epColumn.py
@@ -1,6 +1,104 @@
 from manimlib.imports import *
 
-class ten63(Scene):
+class ep051(Scene):
+    def construct(self):
+        text1 = Text("整数：1、2、3、4、...", font='阿里巴巴普惠体 B',size=0.5).move_to(1.5*LEFT)
+        numberline1=NumberLine(x_min=0,
+                                x_max=5,
+                                include_tip=True,
+                                tick_size=0.1,
+                                tick_frequency=0.5,
+                                color=WHITE
+                      ).move_to(DOWN)
+        self.play(ShowCreation(text1),ShowCreation(numberline1),run_time=3)
+        self.wait(5)
+
+class ep052(Scene):
+    def construct(self):
+        text1 = Text("...")
+        text2 = Text("...")
+        x=-4
+        y=0
+        num=2
+        cl=2
+        scl=0.8
+        bead1=ImageMobject("bead").move_to(np.array([x,y+2,0]))
+        copperCoin1=ImageMobject("copperCoin").move_to(np.array([x,y,0]))
+        bead1.scale(scl)
+        copperCoin1.scale(scl)
+        self.play(FadeIn(bead1),FadeIn(copperCoin1))
+        self.wait(2)
+        for i in range(num):
+            bead=ImageMobject("bead")
+            copperCoin=ImageMobject("copperCoin")
+            bead.scale(scl)
+            copperCoin.scale(scl)
+            bead.move_to(np.array([cl*(i+1)+x,y+2,0]))
+            copperCoin.move_to(np.array([cl*(i+1)+x,y,0]))
+            self.play(FadeInFrom(bead,RIGHT),FadeInFrom(copperCoin,RIGHT),run_time=2)
+            if i==(num-1):
+                text1.next_to(bead,RIGHT)
+                text2.next_to(copperCoin,RIGHT)
+                self.play(ShowCreation(text1),ShowCreation(text2))
+        self.wait(10)
+
+class ep053(Scene):
+    def construct(self):
+        text1 = Text("整数：1、2、3、4、...", font='阿里巴巴普惠体 B',size=0.5)
+        text2 = Text("偶数：2、4、6、8、...", font='阿里巴巴普惠体 B',size=0.5).move_to(DOWN)
+        self.play(ShowCreation(text1),ShowCreation(text2),run_time=2)
+        self.wait(8)
+
+class ep054(Scene):
+    def construct(self):
+        text1 = Text("非负整数：0、1、2、3、...", font='阿里巴巴普惠体 B',size=0.5)
+        text2 = Text("正整数：1、2、3、4、...", font='阿里巴巴普惠体 B',size=0.5)\
+            .move_to(DOWN).align_to(text1,RIGHT)
+        self.play(ShowCreation(text1),ShowCreation(text2),run_time=2)
+        self.wait(10)
+
+class ep055(Scene):
+    def construct(self):
+        text1 = TexMobject("\\frac{1}{1}\\ \\",
+                           "\\frac{1}{2},",
+                           "\\frac{2}{1}\\ \\",
+                           "\\frac{1}{3},",
+                           "\\frac{2}{2},",
+                           "\\frac{3}{1}\\ \\",
+                           "..."
+                           )
+        text2 = TexMobject("1",
+                           "2,",
+                           "3",
+                           "4,",
+                           "5,",
+                           "6",
+                           "...")
+        for i in range(6):
+            text2[i].next_to(text1[i],DOWN)
+        text2[6].next_to(text1[6],3*DOWN)
+        self.play(ShowCreation(text1[0]),run_time=2)
+        self.wait(3)
+        self.play(ShowCreation(text1[1:3]),run_time=2)
+        self.wait(3)
+        self.play(ShowCreation(text1[3:6]),run_time=2)
+        self.wait(3)
+        self.play(ShowCreation(text1[6]),run_time=2)
+        self.wait(10)
+        self.play(ShowCreation(text2),run_time=2)
+        self.wait(10)
+
+class ep056(Scene):
+    def construct(self):
+        text1 = TextMobject(
+            "\\small$$\\frac{1}{1}\\ \\ \\frac{1}{2},\\frac{2}{1}\\ \\ \\frac{1}{3},\\frac{2}{2},\\frac{3}{1}\\ \\ ...$$")
+        text2 = TextMobject(
+            "\\textbf{$$1 2,3 4,5,6 ...$$}",)
+        self.add(text1)
+        self.add(text2.move_to(DOWN))
+        self.wait(5)
+
+class ep031(Scene):
     def construct(self):
         text = TexMobject("10000000000…00","=","{10","^{63}}")
         text[0].set_color(RED)
@@ -15,7 +113,7 @@ class ten63(Scene):
         self.play(Write(text[1:]))
         self.wait(3)
 
-class ChessBoard(Scene):
+class ep041(Scene):
     def construct(self):
         target_list = []
         for i in range(8):
@@ -48,7 +146,7 @@ class ChessBoard(Scene):
             self.wait()
         self.wait()
 
-class pow2(Scene):
+class ep042(Scene):
     def construct(self):
         text = TexMobject("1+2+4+8+...+{2^{63}}", # 0
                           "=",                    # 1
@@ -72,7 +170,7 @@ class pow2(Scene):
         self.play(Write(text[3:5]))
         self.wait(3)
 
-class pow3(Scene):
+class ep043(Scene):
     def construct(self):
         text1 = TextMobject("1斤麦子=10000颗")
         text2 = TextMobject("18446744073709551615颗")
@@ -91,7 +189,7 @@ class pow3(Scene):
         self.wait(5)
 
 
-class pow4(Scene):
+class ep044(Scene):
     def construct(self):
         text1 = TextMobject("1层: 1步")
         text2 = TextMobject("2层: 3步")
@@ -116,7 +214,7 @@ class pow4(Scene):
         self.play(Write(text6))
         self.wait(2)
 
-class pow5(Scene):
+class ep045(Scene):
     def construct(self):
         text1 = Text("1秒=1步", font='义启魔音体')
         text2 = Text("1天=86400秒", font='阿里巴巴普惠体 B')
