1d69f7dcb0c87c8f86ed21962cff3eb106b2b11c
unAlpha <pagzhong@gmail.com>
2020-06-25 23:20:37 +0800

    add 2020-06-25

---
+++ Ag/ZiYuXi/HealthyLifeYears.py
@@ -0,0 +1,28 @@
+from manimlib.imports import *
+
+class HealthyLifeYears(Scene):
+    def construct(self):
+        txt79 = Text("79岁")
+        crl = Circle(radius=1.2)
+        text1 = Text("发达国家人的寿命:77–90岁",
+                    font='宋体',
+                    size=0.5
+            )
+        text2 = Text("发展中国家人的寿命:32–80岁",
+                    font='宋体',
+                    size=0.5
+            )
+
+        tcrlGroup = VGroup(txt79,crl).shift(1*UP)
+        text = VGroup(text1,text2)\
+            .arrange(DOWN,aligned_edge = RIGHT,buff=MED_SMALL_BUFF)\
+            .next_to(tcrlGroup,2*DOWN)
+
+        # self.add(tcrlGroup,text)
+        self.play(FadeInFromLarge(txt79))
+        self.play(ShowCreation(crl))
+        self.wait()
+        self.play(FadeInFromRandomB(text1))
+        self.wait()
+        self.play(FadeInFromRandomB(text2))
+        self.wait()
--- manimlib/mobject/svg/text_mobject.py
+++ manimlib/mobject/svg/text_mobject.py
@@ -28,7 +28,7 @@ class Text(SVGMobject):
         'font': '阿里巴巴普惠体 B',
         'gradient': None,
         'lsh': -1,
-        'size': 1,
+        'size': 0.618,
         'slant': NORMAL,
         'weight': NORMAL,
         't2c': {},
