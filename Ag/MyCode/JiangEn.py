60085532b846bcc2842f27b21017144b244223cf
unAlpha <pagzhong@gmail.com>
2020-07-10 22:07:07 +0800

    add Constellate

--- Ag/MyCode/JiangEn.py
+++ Ag/MyCode/JiangEn.py
@@ -6,6 +6,7 @@ class helixMatrix():
         self.ChartSize = ChartSize
         self.Step = Step
         self.MaxNums = (2*ChartSize+1)**2
+
     # 把数字转成坐标
     def Num2XY(self,Nums):
         NumInCycle = 0
@@ -34,9 +35,8 @@ class helixMatrix():
         if Num >= (2*NumInCycle-1)**2 + NsqareNum*3-2  and Num < (2*NumInCycle-1)**2+NsqareNum*4-3:
             x = - Num + (2*NumInCycle-1)**2 + NsqareNum*3 + NumInCycle - 3
             y = - NumInCycle
-        if x<0 or y<0:
-            raise Exception("超出下(0,0)范围")
         return (x,y)
+
     # 把坐标转成数字    
     def XY2Num(self,x,y):
         if abs(y)>=abs(x):
@@ -55,6 +55,7 @@ class helixMatrix():
                 Num = -y + (2*NumInCycle-1)**2 + NsqareNum*2 + NumInCycle -2
         
         return Num*self.Step-self.Step+self.BeginValu
+
 
     # 90度转角数
     def Num90(self,Num,positiveORnegative):
@@ -79,6 +80,7 @@ class helixMatrix():
                 return self.XY2Num(y,x)
             if y<0 and abs(y)>abs(x):
                 return self.XY2Num(y-1,x+1)
+
     # 180度转角数
     def Num180(self,Num,positiveORnegative):
         if self.Step<0:
@@ -107,8 +109,9 @@ class helixMatrix():
             if x<0 and abs(x)>abs(y):
                 return self.XY2Num(-x,y)
             if y<0 and abs(y)>abs(x):
-                return self.XY2Num(x,-y+1)
+                return self.XY2Num(x,-y+1) 
 
+    # 风车位的角线数
     def JiaoXian(self,Num,positiveORnegative):
         # 仅配合风车位使用
         if self.Step<0:
@@ -147,6 +150,25 @@ class helixMatrix():
             if x>0 and y<0:
                 return self.XY2Num(y-1,x+1)
 
+    # 过滤相连的目标数
+    def FilterNumbers(self, num, positiveORnegative, dnum=2):
+        if not isinstance(num,list):
+            return Exception("需要给个list……")
+        if positiveORnegative == 1:
+            verse = False
+        else:
+            verse = True
+        numSort = sorted(num, reverse = verse)
+        delnumSort = []
+        for i in range(len(numSort)):
+            if i>0:
+                if abs(numSort[i-1]-numSort[i]) <= dnum:
+                    delnumSort.append(numSort[i])
+        for delnum in delnumSort:
+            numSort.remove(delnum)
+        # print(delnumSort)
+        return numSort
+    
     # 风车位推图
     def Windmill(self,Num,positiveORnegative,n):
         if positiveORnegative != 1 and positiveORnegative != -1:
@@ -170,13 +192,37 @@ class helixMatrix():
                     WindmillTaget.append(self.JiaoXian(Num,positiveORnegative)) 
             return WindmillTaget
 
+    # Constellate推图
+    def Constellate(self,Num,positiveORnegative,N,dnum=2):
+        if self.BeginValu!=1:
+            raise Exception("请使用基数1")
+        num11 = self.Num90(Num,positiveORnegative)
+        num21 = self.Num180(Num,positiveORnegative)
+        num22 = self.Num90(num11,positiveORnegative)
+        num31 = self.Num180(num11,positiveORnegative)
+        num32 = self.Num90(num21,positiveORnegative)
+        num33 = self.Num90(num22,positiveORnegative)       
+        num1 = [num11]
+        num2 = [num21,num22]
+        num3 = self.FilterNumbers([num31,num32,num33],positiveORnegative,dnum) 
+        num = [num1,num2,num3]
+        if N>3:
+            for i in range(3+1,N+1):
+                newlist = []
+                for ber1 in num[i-2]:
+                    newlist.append(self.Num90(ber1,positiveORnegative))
+                for ber2 in num[i-3]:
+                    newlist.append(self.Num180(ber2,positiveORnegative))
+                num.append(self.FilterNumbers(newlist,positiveORnegative,dnum))
+        return num[:N]
+
     # 四角推图
     def FourCorners(self,Num,positiveORnegative,N):
         pass
 
 if __name__ == '__main__':
-    BeginValu = 922
-    Step = -1
-    ChartSize = 15
+    BeginValu = 1
+    Step = 1
+    ChartSize = 17
     JinagEnMatrix = helixMatrix(BeginValu,Step,ChartSize)
-    print(JinagEnMatrix.Windmill(807,1,3))
+    print(JinagEnMatrix.Constellate(817,-1,5,2))
