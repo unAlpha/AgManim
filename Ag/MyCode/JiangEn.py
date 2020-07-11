e0ebbd67fe66d9c152c6f93346f90b655fd87b20
unAlpha <pagzhong@gmail.com>
2020-07-11 15:25:14 +0800

    change CW

--- Ag/MyCode/JiangEn.py
+++ Ag/MyCode/JiangEn.py
@@ -57,22 +57,22 @@ class helixMatrix():
         return Num*self.Step-self.Step+self.BeginValu
 
     # 90度转角数
-    def Num90(self,Num,positiveORnegative):
+    def Num90(self,Num,CW):
         if self.Step<0:
-            positiveORnegative=-positiveORnegative
-        if positiveORnegative != 1 and positiveORnegative != -1:
+            CW=-CW
+        if CW != 1 and CW != -1:
             raise Exception("请带参数，正转1，反转-1 ……")
         (x,y) = self.Num2XY(Num)
         if abs(x) == abs(y):
             raise Exception("值位于角线上，无90角转值 ……")
-        if positiveORnegative == -1:
+        if CW == -1:
             if x>=0 and abs(x)>abs(y):
                 return self.XY2Num(y,x)
             if abs(y)>abs(x):
                 return self.XY2Num(-y,-x)
             if x<0 and abs(x)>abs(y):
                 return self.XY2Num(y-1,x+1)
-        if positiveORnegative == 1:
+        if CW == 1:
             if abs(x)>abs(y):
                 return self.XY2Num(-y,-x)
             if y>=0 and abs(y)>abs(x):
@@ -81,17 +81,17 @@ class helixMatrix():
                 return self.XY2Num(y-1,x+1)
 
     # 180度转角数
-    def Num180(self,Num,positiveORnegative):
+    def Num180(self,Num,CW):
         if self.Step<0:
-            positiveORnegative=-positiveORnegative
-        if positiveORnegative != 1 and positiveORnegative != -1:
+            CW=-CW
+        if CW != 1 and CW != -1:
             raise Exception("请带参数，正转1，反转-1 ……")
         (x,y) = self.Num2XY(Num)
 
         if abs(x) == abs(y):
             raise Exception("值位于角线上，无180角转值 ……")
 
-        if positiveORnegative == -1:
+        if CW == -1:
             if x>=0 and abs(x)>abs(y):
                 return self.XY2Num(-x,y)
             if y>=0 and abs(y)>abs(x):
@@ -100,7 +100,7 @@ class helixMatrix():
                 return self.XY2Num(-x-1,y)
             if y<0 and abs(y)>abs(x):
                 return self.XY2Num(x,-y)
-        if positiveORnegative == 1:
+        if CW == 1:
             if x>=0 and abs(x)>abs(y):
                 return self.XY2Num(-x-1,y)
             if y>=0 and abs(y)>abs(x):
@@ -111,15 +111,15 @@ class helixMatrix():
                 return self.XY2Num(x,-y+1) 
 
     # 风车位的角线数
-    def JiaoXian(self,Num,positiveORnegative):
+    def JiaoXian(self,Num,CW):
         # 仅配合风车位使用
         if self.Step<0:
-            positiveORnegative=-positiveORnegative
-        if positiveORnegative != 1 and positiveORnegative != -1:
+            CW=-CW
+        if CW != 1 and CW != -1:
             raise Exception("请带参数，正转1，反转-1 ……")
         (x,y) = self.Num2XY(Num)
         # print((x,y))
-        if positiveORnegative == -1:
+        if CW == -1:
             if x>=0 and y>=0:
                 if x==y:
                     return self.XY2Num(-x+1,-y+1)
@@ -134,7 +134,7 @@ class helixMatrix():
             if x>=0 and y<=0:
                 return self.XY2Num(y,x)
             
-        if positiveORnegative == 1:
+        if CW == 1:
             if x>=0 and y>=0:
                 if abs(y)>abs(x):
                     return self.XY2Num(-y-1,-x-1)
@@ -151,10 +151,10 @@ class helixMatrix():
                 return self.XY2Num(y-1,x+1)
 
     # 过滤相连的目标数
-    def FilterNumbers(self, num, positiveORnegative, dnum=2):
+    def FilterNumbers(self, num, CW, dnum=2):
         if not isinstance(num,list):
             return Exception("需要给个list……")
-        if positiveORnegative == 1:
+        if CW == 1:
             verse = False
         else:
             verse = True
@@ -170,63 +170,63 @@ class helixMatrix():
         return numSort
     
     # 风车位推图
-    def Windmill(self,Num,positiveORnegative,n):
-        if positiveORnegative != 1 and positiveORnegative != -1:
+    def Windmill(self,Num,CW,n):
+        if CW != 1 and CW != -1:
             raise Exception("请带参数，正转1，反转-1 ……")
         if n<1:
             raise Exception("请输入要求的个数")
-        (x,y) = self.Num2XY(Num)
+        (x,y) = self.Num2XY(Num) 
         WindmillTaget = []
         if abs(x)>2*abs(y) or abs(y)>2*abs(x):
             for i in range(n):
                 if i!=0:
-                    WindmillTaget.append(self.Num180(WindmillTaget[i-1],positiveORnegative))
+                    WindmillTaget.append(self.Num180(WindmillTaget[i-1],CW))
                 else:
-                    WindmillTaget.append(self.Num180(Num,positiveORnegative)) 
+                    WindmillTaget.append(self.Num180(Num,CW)) 
             return WindmillTaget
         else:
             for i in range(n):
                 if i!=0:
-                    WindmillTaget.append(self.JiaoXian(WindmillTaget[i-1],positiveORnegative))
+                    WindmillTaget.append(self.JiaoXian(WindmillTaget[i-1],CW))
                 else:
-                    WindmillTaget.append(self.JiaoXian(Num,positiveORnegative)) 
+                    WindmillTaget.append(self.JiaoXian(Num,CW)) 
             return WindmillTaget
 
     # Constellate推图
-    def Constellate(self,Num,positiveORnegative,N,dnum=2):
+    def Constellate(self,Num,CW,N,dnum=2):
         if N<0:
             raise Exception("请输入大于1的段数")
         if self.BeginValu!=1:
             raise Exception("请使用基数1")
-        num11 = self.Num90(Num,positiveORnegative)
-        num21 = self.Num180(Num,positiveORnegative)
-        num22 = self.Num90(num11,positiveORnegative)
-        num31 = self.Num180(num11,positiveORnegative)
-        num32 = self.Num90(num21,positiveORnegative)
-        num33 = self.Num90(num22,positiveORnegative)       
+        num11 = self.Num90(Num,CW)
+        num21 = self.Num180(Num,CW)
+        num22 = self.Num90(num11,CW)
+        num31 = self.Num180(num11,CW)
+        num32 = self.Num90(num21,CW)
+        num33 = self.Num90(num22,CW)       
         num1 = [num11]
-        num2 = self.FilterNumbers([num21,num22],positiveORnegative,dnum) 
-        num3 = self.FilterNumbers([num31,num32,num33],positiveORnegative,dnum) 
+        num2 = self.FilterNumbers([num21,num22],CW,dnum) 
+        num3 = self.FilterNumbers([num31,num32,num33],CW,dnum) 
         num = [num1,num2,num3]
         if N>3:
             for i in range(3+1,N+1):
                 newlist = []
                 for ber1 in num[i-2]:
-                    newlist.append(self.Num90(ber1,positiveORnegative))
+                    newlist.append(self.Num90(ber1,CW))
                 for ber2 in num[i-3]:
-                    newlist.append(self.Num180(ber2,positiveORnegative))
-                num.append(self.FilterNumbers(newlist,positiveORnegative,dnum))
+                    newlist.append(self.Num180(ber2,CW))
+                num.append(self.FilterNumbers(newlist,CW,dnum))
         return num[:N]
 
     # 同位阶
-    def Apposition(self,Num,positiveORnegative):
-        n1 = self.Num180(Num,positiveORnegative)
-        n2 = self.Num90(n1,-positiveORnegative)
-        n3 = self.Num90(n2,-positiveORnegative)
+    def Apposition(self,Num,CW):
+        n1 = self.Num180(Num,CW)
+        n2 = self.Num90(n1,-CW)
+        n3 = self.Num90(n2,-CW)
         
-        m1 = self.Num90(Num,positiveORnegative)
-        m2 = self.Num90(m1,positiveORnegative)
-        m3 = self.Num180(m2,-positiveORnegative)
+        m1 = self.Num90(Num,CW)
+        m2 = self.Num90(m1,CW)
+        m3 = self.Num180(m2,-CW)
 
         if n3 == m3:
             return n3
@@ -234,16 +234,16 @@ class helixMatrix():
             return "无同位阶"
 
     # 四角推图
-    def FourCorners(self,Num,positiveORnegative,N):
+    def FourCorners(self,Num,CW,N):
         if N<0:
             raise Exception("请输入大于1的段数")
-        num11 = self.Num90(Num,positiveORnegative)
-        num12 = self.Num180(num11,-positiveORnegative)
+        num11 = self.Num90(Num,CW)
+        num12 = self.Num180(num11,-CW)
         num = [[num11,num12]]
         if N>1:
             for i in range(1,N):
-                num90 = self.Num90(num[i-1][0],positiveORnegative)
-                num180 = self.Num180(num90,-positiveORnegative)
+                num90 = self.Num90(num[i-1][0],CW)
+                num180 = self.Num180(num90,-CW)
                 num.append([num90,num180])
         return num[:N]
 
@@ -251,35 +251,42 @@ class helixMatrix():
     def Angle(self,num1,num2,step,chartSize,n=6,dnum=2):
         # n为对比的个数
         self.ChartSize = chartSize
-        if num1>num2:
-            self.BeginValu = num1
-            self.Step = -abs(step)
-            num1Windmill = self.Windmill(num2,1,n)
-            self.BeginValu = num2
-            self.Step = abs(step)
-            num2Windmill = self.Windmill(num1,-1,n)
-        else:
+        if num1==num2:
             return "两数相等无夹角"
+        else:
+            nummax = max([num1,num2])
+            nummin = min([num1,num2])
+        self.BeginValu = nummax
+        self.Step = -abs(step)
+        num1Windmill = self.Windmill(nummin,1,n)
+        self.BeginValu = nummin
+        self.Step = abs(step)
+        num2Windmill = self.Windmill(nummax,-1,n)
         # 筛选的数与方向无关
         print(num1Windmill)
         print(num2Windmill)
         numSort = sorted(self.FilterNumbers(num1Windmill,1,dnum)\
             +self.FilterNumbers(num2Windmill,1,dnum))
+        # print(numSort)
         delnumSort = []
         for i in range(len(numSort)):
             if i>0:
                 if abs(numSort[i-1]-numSort[i]) > dnum:
                     delnumSort.append(numSort[i])
-        if numSort[1] in delnumSort:
-            delnumSort.append(numSort[0])
+        delnumSort.append(numSort[0])
+        # print(delnumSort)
         for delnum in delnumSort:
             numSort.remove(delnum)
-        # print(delnumSort)
         return numSort
 
 if __name__ == '__main__':
 
-    # JinagEnMatrix = helixMatrix(7935,1,10)
-    # print("--------------------------------------")
-    # print(JinagEnMatrix.Windmill(7998,-1,7))
-    print(helixMatrix().Angle(8032,7965,1,12,6))
+    # 风车位
+    # print(
+    #     helixMatrix(BeginValu=506, Step=1, ChartSize=10)\
+    #     .Windmill(750, CW=1, n=6)
+    #     )
+
+    # 夹角位
+    print(helixMatrix()\
+        .Angle(1818, 1773, step=1, chartSize=10, n=6))
